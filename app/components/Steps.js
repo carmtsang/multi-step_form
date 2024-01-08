079d38ec0ca0fadffab267f7738354184ba7f70b
carmtsang <carmtsang89@hotmail.com>
2024-01-07 19:06:13 -0800

    add indicators

--- app/components/Steps.js
+++ app/components/Steps.js
@@ -1,9 +1,14 @@
 export default function Steps({ stepNum, totalSteps }) {
   return (
-    <div>
-      <p>
+    <div className="mt-6 flex space-x-8">
+      <p className="text-xs text-gray">
         Step {stepNum} of {totalSteps}
       </p>
+      <div className="flex pace-x-6">
+        <span className="flex w-2.5 h-2.5 me-2.5 bg-purple rounded-full ring-[6px] ring-purple/25" />
+        <span className="flex w-2.5 h-2.5 me-2.5 bg-gray rounded-full" />
+        <span className="flex w-2.5 h-2.5 me-2.5 bg-gray rounded-full" />
+      </div>
     </div>
   );
 }
