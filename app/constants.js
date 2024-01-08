0dbd1d32349afa446d073d43911011c73411304f
carmtsang <carmtsang89@hotmail.com>
2024-01-07 19:08:02 -0800

    add constants

---
+++ app/constants.js
@@ -0,0 +1,23 @@
+export const CARD_TITLE_BUTTON_STEP = {
+  1: {
+    step: 1,
+    title: "Register",
+    buttonText: "Continue",
+  },
+  2: {
+    step: 2,
+    title: "Which topics are you interested in?",
+    buttonText: "Continue",
+  },
+  3: {
+    step: 3,
+    title: "Summary",
+    buttonText: "Confirm",
+  },
+};
+
+export const USER_INTERESTED_TOPICS = [
+  "Software Development",
+  "User Experience",
+  "Graphic Design",
+];
