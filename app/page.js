8565b46b9a8e2f022aac67e04c58fa5d714c0f25
carmtsang <carmtsang89@hotmail.com>
2024-01-29 22:11:46 -0800

    update state

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
 "use client";
 
 import { useState } from "react";
-import Card from "./components/Card";
+
 import Steps from "./components/Steps";
 import { CARD_TITLE_BUTTON_STEP } from "./constants";
+import Card from "./components/Card";
 
 export default function Home() {
-  const [topic, setTopic] = useState("");
+  const [registrantInfo, setRegistrantInfo] = useState({
+    name: "",
+    email: "",
+    topics: [],
+  });
+
   const [registrationStep, setRegistrationStep] = useState({
     step: 1,
     title: "Register",
@@ -14,12 +20,13 @@ export default function Home() {
   });
 
   const allSteps = Object.values(CARD_TITLE_BUTTON_STEP);
-  console.log(allSteps);
 
+  const handleNextStep = () => {};
   return (
     <main className="h-screen flex flex-col items-center justify-center">
       <Card
-        setTopic={setTopic}
+        registrantInfo={registrantInfo}
+        setRegistrantInfo={setRegistrantInfo}
         registrationStep={registrationStep}
         setRegistrationStep={setRegistrationStep}
         allSteps={allSteps}
